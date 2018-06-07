5da1a4aa7179ba98e3600eddabfd140b1bef8559
jasonmpittman <jmpittman@captechu.edu>
2018-06-07 08:01:15 -0400

    completed rank function and w statistic computation

--- wilcoxon-signed-rank.py
+++ wilcoxon-signed-rank.py
@@ -3,7 +3,7 @@
 # Created on 06/4/2018
 # @author: Jason M. Pittman
 # @license: MIT-license
-# Purpose: compute the wilcoxon signed ranked sum statistic  
+# Purpose: compute the wilcoxon signed ranked sum statistic (w- and w+)
 # Explanation: 
 
 def compute_treatment_diff(a, b):
@@ -15,41 +15,53 @@ def compute_treatment_diff(a, b):
     return differences
 
 def rank_differences(differences):
-    ranked_signed = []
-    ranked_abs = []
     ranked = []
+    rank = 1
 
-    for d in differences:
-        ranked_abs.append(abs(d))
-        ranked_signed.append(d)
+    for diff in differences:
+        if diff < 0:
+            ranked.append([abs(diff), '-'])
+        if diff == 0:
+            ranked.append([0, ' '])
+        if diff > 0:
+            ranked.append([abs(diff), '+'])
 
-    ranked_abs.sort()
+    ranked.sort()
 
-    ranked = [ranked_abs, ranked_signed]
+    for r in ranked:
+        r.insert(0, rank)
+        rank += 1
+
     return ranked
 
-def eval_signs():
-    return 0
-
-def compute_w_negative():
+def compute_w_statistics(ranked):
     w_negative = 0
-
-    return w_negative
-
-def compute_w_positive():
     w_positive = 0
+    w = []
 
-    return w_positive
+    for r in ranked:
+        if r[2] == '-':
+            w_negative += r[0]
+        if r[2] == '+':
+            w_positive += r[0]
+
+    w = [w_negative, w_positive]
+
+    return w
 
 def main():
     treatment_a = [2.5, 3.5, 2.9, 2.1, 6.9, 2.4, 4.9, 6.6, 2.0, 2.0, 5.8, 7.5]
     treatment_b = [4.0, 5.6, 3.2, 1.9, 9.5, 2.3, 6.7, 6.0, 3.5, 4.0, 8.1, 19.9]
+    w = []
 
     differences = compute_treatment_diff(treatment_a, treatment_b)
     ranked = rank_differences(differences)
+    w = compute_w_statistics(ranked)
 
-    print("The differences are: {0}".format(differences))
-    print("\n")
-    print("The ranked differences are: {0}".format(ranked))
+    #print("The differences are: {0}".format(differences))
+    #print("\n")
+    #print("The ranked differences are: {0}".format(ranked))
+    #print("\n")
+    print("The W- and W+ are: {0} ; {1}".format(w[0], w[1]))
 
 main()
